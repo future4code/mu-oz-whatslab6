ed0a450155ccfb3b8d31e6fb0bdb67dbeb4aa8f5
zak almeida <phillipszak@hotmail.com>
2021-05-07 21:46:34 -0300

    Tirei os componentes e fiz as funções pricipais

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,18 +1,66 @@
 import React from 'react';
 import './App.css';
-import QuadroDeMenssagens from './components/QuadroDeMenssagens/QuadroDeMenssagens.js';
-import InputDeTexto from './components/InputDeTexto/InputDeTexto.js';
 
-function App() {
-  return (
-    <div className="App">
 
-      <QuadroDeMenssagens/>
 
-      <InputDeTexto/>
+class Whatslab extends React.Component {
+  state = {
+    valorUsuario: "",
+    valorTexto: "",
+    conversa: []
+  }
+  
+  onChangeUsuario = (e) =>{
+    this.setState({valorUsuario: e.target.value})
+  }
 
-    </div>
-  );
+  onChangeTexto = (e) =>{
+    this.setState({valorTexto: e.target.value})
+  }
+
+  onClickEnviar = () =>{
+    if (this.state.valorUsuario && this.state.valorTexto) {
+    const novaMensagem ={
+      usuario: this.state.valorUsuario,
+      texto: this.state.valorTexto,
+    }
+ 
+    const novaConversa = [...this.state.conversa, novaMensagem]
+
+    this.setState({conversa: novaConversa})
+    this.setState({valorUsuario:"", valorTexto:""})
+   }
+  }
+
+  render () {
+
+    const nossaConversa = this.state.conversa.map((props) => {
+        
+      return(
+        <div>
+           <p className="fala">{props.usuario} diz:</p><p>{props.texto}</p>
+        </div>
+      )
+
+    })
+
+    return(
+      <div>
+
+        <div id="main">
+           {nossaConversa}
+        </div>
+
+        <div id="form">
+          <p className="fala">Usuário:</p>
+          <input value={this.state.valorUsuario} placeHolder="Seu nome" type="text" id="nome" onChange={this.onChangeUsuario}/>
+          <input value={this.state.valorTexto} placeHolder="Escreva sua mensagem aqui" type="text" id="texto" onChange={this.onChangeTexto}/>
+          <button onClick={this.onClickEnviar}>Enviar</button>
+        </div>
+
+      </div>
+
+    )}
 }
 
-export default App;
+export default Whatslab;
