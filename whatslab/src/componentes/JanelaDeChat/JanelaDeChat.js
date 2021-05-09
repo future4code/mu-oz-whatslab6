67f3782d174c2cb632235a5bff8377a01e610100
eddvaz <edivaldo.vaz@hotmail.com>
2021-05-08 23:38:55 -0300

    estilizacao-concluida

--- whatslab/src/componentes/JanelaDeChat/JanelaDeChat.js
+++ whatslab/src/componentes/JanelaDeChat/JanelaDeChat.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import './JanelaDeChat.css';
+import styled from 'styled-components'
 
+const BalaoDeMensagem = styled.div`
+    background-color: #DDF7C8;
+    padding: 0.9em 0.8em;
+    border-radius: 0.5em;
+    font-weight: 450;
+    line-height: 1.3;
+    display:flex;
+    align-self: flex-end;
+    max-width: 60%;
+    min-width: 8%;
+    margin-bottom: 1em;
+`
 
 class JanelaDeChat extends React.Component {
     state = {
@@ -36,26 +49,31 @@ class JanelaDeChat extends React.Component {
       const nossaConversa = this.state.conversa.map((props) => {
           
         return(
+        <BalaoDeMensagem>  
           <div>
-             <p className="fala">{props.usuario} diz:</p><p>{props.texto}</p>
+             <p className="fala"> {props.usuario} </p><p>{props.texto}</p>
           </div>
+        </BalaoDeMensagem>
         )
   
       })
   
       return(
+        
         <div>
-  
+          
           <div id="main">
              {nossaConversa}
           </div>
-  
+
+          
           <div id="form">
             <p className="fala">Usuário:</p>
-            <input value={this.state.valorUsuario} placeHolder="Seu nome" type="text" id="nome" onChange={this.onChangeUsuario}/>
-            <input value={this.state.valorTexto} placeHolder="Escreva sua mensagem aqui" type="text" id="texto" onChange={this.onChangeTexto}/>
-            <button onClick={this.onClickEnviar}>Enviar</button>
+            <input className="textarea" value={this.state.valorUsuario} placeHolder="Seu nome" type="text" id="nome" onChange={this.onChangeUsuario}/>
+            <input className="textarea" value={this.state.valorTexto} placeHolder="Digite aqui" type="text" id="texto" onChange={this.onChangeTexto}/>
+            <img className="enviar" src="https://cdn4.iconfinder.com/data/icons/flat-circle-content/800/circle-content-send-512.png" onClick={this.onClickEnviar}></img>
           </div>
+          
   
         </div>
   
